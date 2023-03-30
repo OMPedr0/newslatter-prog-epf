9625cecaad3b928e10b8bf9c9a3e3652f69b099c
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2023-03-30 21:45:27 +0100

    Update index.tsx

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-
+import Image from 'next/image';
+import logoprincipal from '../imgs/logoprinciapal.png';;
 const Register = () => {
 
     const [email, setEmail] = useState('');
@@ -24,44 +25,48 @@ const Register = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold mb-8">Register</h1>
-            <form className="w-full max-w-sm" onSubmit={handleRegister}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="email"
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+        <div className="min-h-screen flex justify-center items-center">
+            <div className="w-full max-w-md px-8 py-6 bg-gray-400 rounded-lg shadow-md">
+                <div className="flex justify-center items-center mb-4">
+                    <Image src={logoprincipal} alt="Logo" className=" mr-2" />
                 </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
-                        Password
-                    </label>
-                    <input
-                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password"
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="flex items-center justify-between">
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                    >
-                        Register
-                    </button>
-                </div>
-            </form>
+                <form className="w-full max-w-sm" onSubmit={handleRegister}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button
+                            className="bg-zinc-500 hover:bg-zinc-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit"
+                        >
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
